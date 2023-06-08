2b1e5f795bdba6ef821bd961a738cd358da03ba6
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-06-08 19:40:03 -0300

    Application routes - Capturing query parameters

--- 01-fundamentos-nodejs/src/routes.js
+++ 01-fundamentos-nodejs/src/routes.js
@@ -9,6 +9,8 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
+      console.log(req.query)
+
       const users = database.select('users')
     
       return res.end(JSON.stringify(users))
@@ -32,10 +34,29 @@ export const routes = [
     }
   },
   {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email
+      })
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
     method: 'DELETE',
     path: buildRoutePath('/users/:id'),
     handler: (req, res) => {
-      return res.end()
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
     },
-  }
+  },
 ]
