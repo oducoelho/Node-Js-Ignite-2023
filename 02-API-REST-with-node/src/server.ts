b22612555616f866801dad8c01a85573e73ab6b0
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-06-21 17:57:46 -0300

    database: Handling env with Zod

--- 02-API-REST-with-node/src/server.ts
+++ 02-API-REST-with-node/src/server.ts
@@ -1,19 +1,22 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 // GET, PORT, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running!!!')
